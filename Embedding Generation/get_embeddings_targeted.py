e79cfe7ff575887503555e80ade015fb0e81f96e
neelgupta2112 <74472804+neelgupta2112@users.noreply.github.com>
2025-12-20 19:07:17 -0800

    changes to embeddings script to account for new dataframe

--- Embedding Generation/get_embeddings_targeted.py
+++ Embedding Generation/get_embeddings_targeted.py
@@ -8,7 +8,7 @@ import json, gzip, os, string, boto3
 MODEL_DIR = "./finetuned_modernbert-literary-mlm"
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 S3_BUCKET = "ppa-embeddings-bucket"
-S3_PREFIX = "target_embeddings/"
+S3_PREFIX = "target_embeddings_expanded/"
 LOCAL_TMP = "tmp_batches"
 CONTEXT_CHARS = 100
 BATCH_SIZE = 200
@@ -28,12 +28,12 @@ STOPWORDS = {
 
 # ---------------- LOAD DATA ----------------
 df = pd.read_csv("Data/ppa_corpus_2025-02-03_1308/ppa_keyword_db.csv")
-df = df[["page_id", "form", "spelling", "page_text", "work_id", "title", "author", "pub_year"]]
+df = df[["page_id", "poetic_form", "spelling", "page_text", "work_id"]] # "title", "author", "pub_year"]]
 
 # map form -> list of spellings
 form_to_spellings = {
     f: [s.lower().strip(string.punctuation) for s in spellings]
-    for f, spellings in df.groupby("form")["spelling"].agg(list).items()
+    for f, spellings in df.groupby("poetic_form")["spelling"].agg(list).items()
 }
 
 # ---------------- GROUP BY PAGE ----------------
@@ -41,8 +41,8 @@ pages = df.groupby("page_id", sort=False).agg({
     "page_text": "first",
     "spelling": lambda x: list(x),
     "work_id": "first",
-    "title": "first",
-    "author": "first",
+    # "title": "first",
+    # "author": "first",
     "pub_year": "first"
 }).reset_index()
 
@@ -155,10 +155,10 @@ for batch_idx in range(num_batches):
                     json_obj = {
                         "page_id": page_id,
                         "work_id": row["work_id"],
-                        "title": row["title"],
-                        "author": row["author"],
-                        "pub_year": row["pub_year"],
-                        "form": form_value,
+                        # "title": row["title"],
+                        # "author": row["author"],
+                        # "pub_year": row["pub_year"],
+                        "poetic_form": form_value,
                         "spelling": u["word"],
                         "char_start": start,
                         "char_end": end,
